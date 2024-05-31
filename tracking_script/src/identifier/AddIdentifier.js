f435866807b4c834d113fe39c015c9e90a96d910
Will Ondrik <wondrik@bcit.ca>
2024-05-31 10:11:13 -0700

    Template files

---
+++ tracking_script/src/identifier/AddIdentifier.js
@@ -0,0 +1,6 @@
+
+function addIdentifier() {
+
+}
+
+export default addIdentifier;
--- tracking_script/src/index.js
+++ tracking_script/src/index.js
@@ -1,9 +1,10 @@
+import addIdentifier from './identifier/AddIdentifier.js'
 import trackClicks from './events/TrackClicks.js'
 import trackContentInteraction from './events/TrackContentInteraction.js'
 import trackDownloads from './events/TrackDownloads.js'
 // import trackErrors from './events/TrackErrors.js'
 import trackForms from './events/TrackForms.js'
-import trackHovers from './events/TrackHovers.js'
+//import trackHovers from './events/TrackHovers.js'
 import trackPageResizes from './events/TrackPageResizes.js'
 import trackPageTransitions from './events/TrackPageTransitions.js'
 import trackScrollDepth from './events/TrackScrollDepth.js'
@@ -14,11 +15,12 @@ import trackSession from './events/TrackSession.js'
  * 
  */
 function init() {
+    addIdentifier();
     trackClicks();
     trackContentInteraction();
     trackDownloads();
     trackForms();
-    trackHovers();
+   //  trackHovers();
     trackPageResizes();
     trackPageTransitions();
     trackScrollDepth();
