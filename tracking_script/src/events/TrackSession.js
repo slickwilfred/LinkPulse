c4d713257b19e97e7f2d8d4a51b9b1e53cb4ee22
Will Ondrik <wondrik@my.bcit.ca>
2024-06-25 20:25:49 -0700

    Keeps track of the session duration

--- tracking_script/src/events/TrackSession.js
+++ tracking_script/src/events/TrackSession.js
@@ -1,6 +1,20 @@
+import sendData from "../services/apiService";
+
+const sessionStart = Date.now();
 
 function trackSession() {
-
+    window.addEventListener('unload', function() {
+        const sessionEnd = Date.now();
+        const sessionDuration = sessionEnd - sessionStart;
+        const data = {
+            eventType: 'sessionEnd',
+            duration: sessionDuration,
+            start: new Date(sessionStart).toISOString(),
+            end: new Date(sessionEnd).toISOString(),
+            url: window.location.href
+        }
+        sendData(data, 'sessionDuration')
+    })
 }
 
 export default trackSession;
