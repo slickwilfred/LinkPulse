899fb505c31fdc57042de5a45d03a078a11538b1
Will Ondrik <wondrik@my.bcit.ca>
2024-06-25 19:01:00 -0700

    Created middleware to validate: - JWT tokens - origin and referrer

--- tracking_backend/src/middleware/tracking_middleware.go
+++ tracking_backend/src/middleware/tracking_middleware.go
@@ -1,33 +1,64 @@
-package middlware
+package middleware
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
-	"tracking_backend/src/tokens/jwt_token"
 )
 
 type Tracking_Middleware struct {
-	JWTSecretKey    string
+	JWTSecretKey    []byte
 	AllowedOrigin   string
 	AllowedReferrer string
+	jwt.RegisteredClaims
 }
 
-var secretKey = []byte("thisneedstochane")
 func NewMiddleware(secretKey, allowedOrigin, allowedReferrer string) *Tracking_Middleware {
 	return &Tracking_Middleware{
-		JWTSecretKey:    secretKey,
+		JWTSecretKey:    []byte(secretKey),
 		AllowedOrigin:   allowedOrigin,
 		AllowedReferrer: allowedReferrer,
 	}
 }
 
-func (tm *Tracking_Middleware) VerifyToken(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request)) {
-		cookie, err := r.Cookie("auth")
+func (tm *Tracking_Middleware) VerifyToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cookie, err := c.Cookie("auth_token")
+		if err != nil {
+			c.AbortWithStatusJSON(403, gin.H{"error": "No token provided"})
+			return
+		}
+
+		token, err := jwt.ParseWithClaims(cookie, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return tm.JWTSecretKey, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Invalid token"})
+			return
+		}
+
+		c.Set("claims", token.Claims)
+		c.Next()
 	}
 }
 
-func (tm *Tracking_Middleware) ValidateOriginAndReferrer (next http.Handler) http.Handler {
+func (tm *Tracking_Middleware) ValidateOriginAndReferrer() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		origin := c.Request.Header.Get("Origin")
+		referer := c.Request.Header.Get("Referer")
 
+		if origin != tm.AllowedOrigin || !strings.HasPrefix(referer, tm.AllowedReferrer) {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Forbidden"})
+			return
+		}
+
+		c.Next()
+	}
 }
-
