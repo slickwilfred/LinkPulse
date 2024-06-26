aa969e8aa299faf24ae5e9001f34b26fcb4830f4
Will Ondrik <wondrik@my.bcit.ca>
2024-06-25 20:23:53 -0700

    Tracks mouse hovers and sends related data to the server

--- tracking_script/src/events/TrackHovers.js
+++ tracking_script/src/events/TrackHovers.js
@@ -1,6 +1,18 @@
 
 function trackHovers() {
-
+    document.querySelectorAll('.track-hover').forEach(function(element){
+        element.addEventListener('mouseenter', function(event){
+            const data = {
+                eventType: 'hover',
+                element: event.target.tagName,
+                id: event.target.id,
+                classes: event.target.className,
+                textContent: event.target.textContent,
+                timestamp: new Date().toISOString()
+            }
+            sendData(data, 'mousehover')
+        })
+    })
 }
 
 export default trackHovers;
