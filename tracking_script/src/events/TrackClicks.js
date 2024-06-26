dfa8e272b0ad5d49479968604802344ffe10ae1d
Will Ondrik <wondrik@my.bcit.ca>
2024-06-25 20:23:30 -0700

    Adds an event listener and extracts click event data before sending it to the server

--- tracking_script/src/events/TrackClicks.js
+++ tracking_script/src/events/TrackClicks.js
@@ -1,10 +1,21 @@
+import sendData from "../services/apiService";
 
 
 /**
  * 
  */
 function trackClicks () {
-
+    document.addEventListener('click', function(event) {
+        const data = {
+            eventType: 'click',
+            element: event.target.tagName,
+            classes: event.target.className,
+            id: event.target.id,
+            textContent: event.target.textContent,
+            timestamp: new Date().toISOString()
+        }
+        sendData(data, 'clicks')
+    })
 }
 
 export default trackClicks;
